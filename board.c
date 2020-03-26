576a4ddf6f5daa7db3f5af7c7e513133384bb3de
Yutong Zhang <zhangyt2000@gmail.com>
2020-03-26 11:27:20 -0700

    Complete new_Board, print_Board and delete_Board

--- board.c
+++ board.c
@@ -1,32 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "board.h"
 
+#define EMPTY '.'
+#define INDEX(X, Y) X * this->width + Y
+
 typedef struct Board {
+    bool win;
     int width;
     int height;
-    bool state;
-    char** checker;
-    void (*put)(int);
-    void (*print)(void);
-    void (*clear)(void);
-    bool (*getstate)(void);
+    char* checker;
 } Board;
 
-void put(int col) {
+void checkwin(Board * this) {
 }
 
-void print(void) {
+void print_Board(Board * this) {
+    for (int row = 0; row < this->height; row++) {
+        for (int col = 0; col < this->width; col++) {
+            printf("%c ", this->checker[INDEX(row, col)]);
+        }
+        printf("\n");
+    }
+    for (int col = 0; col < this->width; col++) {
+        printf("%d ", col + 1);
+    }
 }
 
-void clear(void) {
+void clear_Board(Board * this) {
 }
 
-bool getstate(void) {
+void put(Board * this, int col, char chess) {
 }
-
 
 Board * new_Board(int width, int height) {
+    Board * this = malloc(sizeof(Board));
+    this->win = false;
+    this->width = width;
+    this->height = height;
+    this->checker = malloc(sizeof(char) * width * height);
+
+    for (int row = 0; row < height; row++) {
+        for (int col = 0; col < width; col++) {
+            this->checker[INDEX(row, col)] = EMPTY;
+        }
+    }
+    return this;
 }
 
 void delete_Board(Board ** toDelete) {
+    Board * this = *toDelete;
+    free(this->checker);
+    free(this);
+    *toDelete = NULL;
 }
--- board.h
+++ board.h
@@ -3,6 +3,14 @@
 
 typedef struct Board Board;
 
+void checkwin(Board * this);
+
+void print_Board(Board * this);
+
+void clear_Board(Board * this);
+
+void put(Board * this, int col, char chess);
+
 Board * new_Board(int width, int height);
 
 void delete_Board(Board ** toDelete);

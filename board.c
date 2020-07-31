bcc1bff020b516f89848b655d7af815f6f9ff445
Yutong Zhang <zhangyt2000@gmail.com>
2020-07-31 16:42:58 -0700

    Complete checkwin with its helper functions

--- board.c
+++ board.c
@@ -3,18 +3,139 @@
 #include <stdbool.h>
 #include "board.h"
 
+#define GOAL 4
 #define EMPTY '.'
 #define NEWLINE "\n"
 #define INDEX(X, Y) X * this->width + Y
 
-typedef struct Board {
-    bool win;
-    int width;
-    int height;
-    char* checker;
-} Board;
+static bool check_Hori(Board * this, int row, int col, char chess) {
+    int count = 0;
+    int colIndex = (col - GOAL + 1) > 0 ? (col - GOAL + 1) : 0;
+    int colLimit = (col + GOAL) < this->width ? (col + GOAL) : this->width;
 
-void checkwin(Board * this) {
+    while (colIndex < colLimit) {
+        if (this->checker[INDEX(row, colIndex)] == chess) {
+            count += 1;
+        } else {
+            count = 0;
+        }
+
+        if (count == GOAL) {
+            return true;
+        }
+
+        colIndex += 1;
+    }
+
+    return false;
+}
+
+static bool check_Vert(Board * this, int row, int col, char chess) {
+    int count = 0;
+    int rowIndex = (row + GOAL - 1) < this->height ? 
+        (row + GOAL - 1) : (this->height - 1);
+    int rowLimit = (row - GOAL) > -1 ? (row - GOAL) : -1;
+
+    while (rowIndex > rowLimit) {
+        if (this->checker[INDEX(rowIndex, col)] == chess) {
+            count += 1;
+        } else {
+            count = 0;
+        }
+
+        if (count == GOAL) {
+            return true;
+        }
+
+        rowIndex -= 1;
+    }
+
+    return false;
+}
+
+static bool check_Diag(Board * this, int row, int col, char chess) {
+    // TODO
+    int leftCount = 0;
+    int rightCount = 0;
+    int leftRowIndex;
+    int rightRowIndex;
+
+    int rowIndex = (row + GOAL - 1) < this->height ?
+        (row + GOAL - 1) : (this->height - 1);
+    int rowLimit = (row - GOAL) > -1 ? (row - GOAL) : -1;
+
+    int leftColIndex = (col - GOAL + 1) > 0 ? (col - GOAL + 1) : 0;
+    int leftColLimit = (col + GOAL) < this->width ?  (col + GOAL) : this->width;
+
+    int rightColIndex = (col + GOAL - 1) < this->width ?
+        (col + GOAL - 1) : (this->width - 1);
+    int rightColLimit = (col - GOAL) > -1 ? (col - GOAL) : -1;
+
+    if (rowIndex - row < col - leftColIndex) {
+        leftColIndex = col + row - rowIndex;
+        leftRowIndex = rowIndex;
+    } else {
+        leftRowIndex = row + col - leftColIndex;
+    }
+    
+    if (rowIndex - row < rightColIndex - col) {
+        rightColIndex = col + rowIndex - row;
+        rightRowIndex = rowIndex;
+    } else {
+        rightRowIndex = row + rightColIndex - col;
+    }
+
+    while (leftColIndex < leftColLimit && leftRowIndex > rowLimit) {
+        if (this->checker[INDEX(leftRowIndex, leftColIndex)] == chess) {
+            leftCount += 1;
+        } else {
+            leftCount = 0;
+        }
+
+        if (leftCount == GOAL) {
+            return true;
+        }
+
+        /*printf("leftRowIndex: %d, leftColIndex: %d, leftCount: %d\n",*/
+               /*leftRowIndex, leftColIndex, leftCount);*/
+        leftRowIndex -= 1;
+        leftColIndex += 1;
+        printf("leftCount: %d\n", leftCount);
+    }
+
+    while (rightColIndex > rightColLimit && rightRowIndex > rowLimit) {
+        if (this->checker[INDEX(rightRowIndex, rightColIndex)] == chess) {
+            rightCount += 1;
+        } else {
+            rightCount = 0;
+        }
+
+        if (rightCount == GOAL) {
+            return true;
+        }
+
+        /*printf("rightRowIndex: %d, rightColIndex: %d, rightCount: %d\n",*/
+                /*rightRowIndex, rightColIndex, rightCount);*/
+        rightRowIndex -= 1;
+        rightColIndex -= 1;
+    }
+    
+    return false;
+}
+
+void checkwin(Board * this, int colNo) {
+    int row = 0;
+    int col = colNo - 1;
+
+    while (this->checker[INDEX(row, col)] == EMPTY) {
+        row += 1;
+    }
+
+    char chess = this->checker[INDEX(row, col)];
+
+    this->win = check_Hori(this, row, col, chess) ||
+        check_Vert(this, row, col, chess) ||
+        check_Diag(this, row, col, chess);
 }
 
 void print_Board(Board * this) {

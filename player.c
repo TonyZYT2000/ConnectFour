df79d4787026a5d290df85e1481de8a5193f16d5
Yutong Zhang <zhangyt2000@gmail.com>
2020-07-31 18:22:19 -0700

    Complete new_Player and human_Move in player.c

--- player.c
+++ player.c
@@ -3,3 +3,37 @@
 #include "player.h"
 
 
+Player * new_Player(char chess, Board * board, void (*move)(void *)) {
+    Player * this = malloc(sizeof(Player));
+    this->chess = chess;
+    this->board = board;
+    this->move = move;
+
+    return this;
+}
+
+void human_Move(void * player) {
+    int col = 0;
+    Player * this = (Player*) player;
+
+    printf("Please enter a column number: ");
+    scanf("%d", &col);
+    
+    while (col < 1 || col > this->board->width) {
+        printf("Invalid column number! Please reenter a column number: ");
+        scanf("%d", &col);
+    }
+    
+    bool success = put(this->board, col, this->chess);
+    while (!success) {
+        printf("That col is full!\n");
+        printf("Plese reenter a column number: ");
+        scanf("%d", &col);
+
+        while (col < 1 || col > this->board->width) {
+            printf("Invalid column number! Please reenter a column number: ");
+            scanf("%d", &col);
+        }
+        success = put(this->board, col, this->chess);
+    }
+}
